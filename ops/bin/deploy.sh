03441810563f50592f1b9ab4f31b3258a89858dc
iris.hou <xinzhu.hou@gmail.com>
2019-07-19 09:53:22 +1000

    Change command, remove parameter

--- ops/bin/deploy.sh
+++ ops/bin/deploy.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 set -euo pipefail
-ktmpl $(pwd)/ops/deploy/deployment.yml \
---parameter SERVICE_NAME iris-hello-world | kubectl apply -f -
+kubectl apply -f ./ops/deploy/deployment.yml -n training
