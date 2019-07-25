573abf4aaf27d4bc67571f3573f93c777bbc6717
iris.hou <xinzhu.hou@gmail.com>
2019-07-25 15:30:57 +1000

    Remove slash

--- ops/bin/test.sh
+++ ops/bin/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker build -f /Dockerfile.test -t iris-hello-world .
+docker build -f Dockerfile.test -t iris-hello-world .
 
 docker run --rm iris-hello-world:latest ./gradlew test
